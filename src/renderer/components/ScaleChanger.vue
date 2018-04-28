25988e1d0a59b3bb74b8b1ed72cd3807c0661c01
Senhung Wong <alexwongsenhung@gmail.com>
2018-04-27 20:24:28 -0700

    Scale Changer

--- src/renderer/components/ScaleChanger.vue
+++ src/renderer/components/ScaleChanger.vue
@@ -1,14 +1,23 @@
 <template>
     <v-card flat class="my-2">
-        <v-card-title primary-title>
-            Scale Changer
-        </v-card-title>
+        <v-tabs fixed-tabs height="35px">
+            <v-tab @click="changeScale(4.33)">
+                4.33
+            </v-tab>
+            <v-tab @click="changeScale(4.00)">
+                4.00
+            </v-tab>
+        </v-tabs>
     </v-card>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        changeScale(scale) {
+            this.$store.commit('changeScale', scale)
+        }
+    }
 }
 </script>
 
